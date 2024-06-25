34e322038d1857159e8d05fde0e78fb48372b047
Clauzinha983 <scremimc@gmail.com>
2024-06-25 21:16:49 +0000

    alterei o script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "No Brasil, em 2022, a média nacional apontou que 31,2% dos adolescentes estavam com excesso de peso. Qual o primeiro pensamento?",
         alternativas: [
-            "Alternativa 1", 
-            "Alternativa 2"
+            "Isso é preocupante!", 
+            "Ficou com receio de estar passando por essa situação."
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "No Brasil, em 2022, a média nacional apontou que 31,2% dos adolescentes estavam com excesso de peso. Qual o primeiro pensamento?",
         alternativas: [
-            "Alternativa 1", 
-            "Alternativa 2"
+            "Isso é bom!", 
+            "Pensou que pelo  menos agora os adolescentes não tem mais problemas para se alimentar."
         ]
     } 
 ];
